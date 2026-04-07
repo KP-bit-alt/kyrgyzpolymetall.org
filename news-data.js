5111ec6f1fbbd88e1213e5b3378655b22fdeb6eb
KP-bit-alt <kutman.pamir@gmail.com>
2026-04-08 00:35:38 +0600

    Update news-data.js

--- news-data.js
+++ news-data.js
@@ -1,17 +1,19 @@
 // Add every news item you want to display on the page inside this array.
 // All items in this file will appear in the News section.
 // Keep the same field names when adding new entries.
+  export const newsItems = [
+  {
+    id: 1,
+    date: '03.04.2026',
+    titleRu: 'ОсОО «Кыргызполиметалл» подписало Меморандум о взаимопонимании с Кыргызско-Японской экономической ассоциацией',
+    titleKg: '«Кыргызполиметалл» ЖЧКсы Кыргыз-Япон экономикалык ассоциациясы менен өз ара түшүнүшүү жөнүндө меморандумга кол койду',
+    titleEn: 'Kyrgyzpolymetall LLC signed a Memorandum of Understanding with the Kyrgyz-Japanese Economic Association',
 
-export const newsItems = [
-  // Example structure:
-  // {
-  //   id: 1,
-  //   date: '29.03.2026',
-  //   titleRu: 'Заголовок новости',
-  //   titleKg: 'Жаңылыктын аталышы',
-  //   titleEn: 'News title',
-  //   excerptRu: 'Краткое описание новости.',
-  //   excerptKg: 'Жаңылыктын кыскача сүрөттөлүшү.',
-  //   excerptEn: 'Short description of the news item.'
-  // }
+    excerptRu: '3 апреля 2026 года в Бишкеке ОсОО «Кыргызполиметалл» и Кыргызско-Японская экономическая ассоциация подписали меморандум о взаимопонимании по совместной разработке горнорудных месторождений.',
+    excerptKg: '2026-жылдын 3-апрелинде Бишкекте «Кыргызполиметалл» ЖЧКсы менен Кыргыз-Япон экономикалык ассоциациясы тоо-кен тармагындагы рудалык кендерди биргелешип өздөштүрүү боюнча меморандумга кол коюшту.',
+    excerptEn: 'On 3 April 2026 in Bishkek, Kyrgyzpolymetall LLC and the Kyrgyz-Japanese Economic Association signed a memorandum of understanding on the joint development of ore deposits.',
+
+    image: 'japankpm.jpg'
+  }
 ];
+
